d1a2a253265319049852b981b7b85093dff869a0
Brandon Spear <BowDownToTheEpicTurkey@gmail.com>
2022-09-19 20:57:18 -0700

    it will update UI'

--- TESTS.md
+++ TESTS.md
@@ -1,10 +1,29 @@
 Describe: Game()
-Test: It will create and instance a Game object.
-Code: Let TicTacToe = new Game();
-Expected Output: undefined
-Result: Undefined
 
-Test: It will show TicTacToe as a Game Object
+Test: It will show TicTacToe as a Game Object.
 Code: TicTacToe;
 Expected Output: Game {players: {…}, currentId: 0}
 Result: Game {players: {…}, currentId: 0}
+
+Test: It will show player count.
+Code:TicTacToe.currentId
+Expected: "0"
+Result: "0"
+
+Description: assignPlayerID ()
+Test: It will save player ID.
+Code:TicTacToe.assignPlayerId ();
+Expected: "1"
+Result: "1"
+
+Description: Player (First Name)
+Test: It will create a player. 
+Code:let player1 = new Player (Bai)
+Expected: { "firstName": "Bai", "mark": "X" }
+Result: { "firstName": "Bai", "mark": "X" }
+
+Description: getMarker()
+Test: It will return the correct marker for the correct player
+Code: player1.getMarker();
+Expected: "Bai's marker is X"
+Result: "Bai's marker is X"
--- js/scripts.js
+++ js/scripts.js
@@ -6,25 +6,36 @@ function Game() {
 }
 
 Game.prototype.addPlayer = function(player) {
-  player.id = this.assignPlayer();
-  this.currentId[player.Id]= player
+  player.id = this.assignPlayerId();
+  this.players[player.Id]= player;
 }
 
-Game.prototype.assignPlayer = function() {
-  this.currentId += 1;
-  return this.currentId;
-}
+Game.prototype.assignPlayerId = function() {
+  if ( this.currentId === "0" || this.currentId === "1") {
+    this.currentId += 1;
+    return this.currentId;
+  } else {
+    return {
+      error: true,
+      message:"There can only be two players"
+    };
+  }
+} 
 
-
-
+Game.prototype.findPlayers = function(id) {
+  if (this.players[id] !== undefined) {
+    return this.players[id];
+  }
+  return false;
+};
 
 //Business Logic for Player
 
-// function Player(firstName) {
-//   this.firstName = firstName;
-//   this.mark = mark;
-// }  
+function Player(firstName) {
+  this.firstName = firstName;
+  this.mark = "X";
+}  
 
-// Player.prototype = function() {
-//   return this.firstname +"'s" + "marker is" + this.mark;
-// }
+Player.prototype.getMarker = function() {
+  return this.firstName +"'s" + " marker is " + this.mark;
+}
